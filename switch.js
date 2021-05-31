9bf4960f1848a925efc23b112d5f5b8e831104b5
Amit Boruah <66059196+amitboruah@users.noreply.github.com>
2021-05-31 08:21:57 +0530

    switch case

---
+++ switch.js
@@ -0,0 +1,17 @@
+var user='tester'
+
+switch (user) {
+    case 'developer':
+        console.log("he is a deveopler");
+        break;
+    case 'tester':
+         console.log("he is a Tester");
+         break;
+    case 'consumer':
+        console.log("he is a consumer");
+        break;
+    
+    default:
+        console.log("he is a guest");
+        break;
+}
